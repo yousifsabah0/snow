1007d947d08a802c5fd81f09b29aa89ee72efd83
Yousif Sabah <iyousifsabah@gmail.com>
2024-07-26 16:30:24 +0300

    dmenu: configurations and themes

---
+++ dmenu/themes/earthy.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+    /*               fg         bg       */
+    [SchemeNorm] = { "#B3B8B5", "#0F0F16" },
+    [SchemeSel]  = { "#8B9494", "#7A6C56" },
+    [SchemeOut]  = { "#0F0F16", "#4A463D" },
+};
---
+++ dmenu/themes/noise.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+    /*               fg         bg       */
+    [SchemeNorm] = { "#404F83", "#1A191A" },
+    [SchemeSel]  = { "#4B5F9D", "#181919" },
+    [SchemeOut]  = { "#1A191A", "#191B1C" },
+};
